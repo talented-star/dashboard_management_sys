ca20313607af6e89327f84cbf60c7fd482ccf624
talented-star <zhiqiang9981@gmail.com>
2024-08-09 10:10:43 +0900

    feat: Update the config
    Update the tailwind config for using the font style.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        mono: ['Courier New', 'monospace'],
+        robot: ["Roboto Condensed", 'sans-serif']
+      },
     },
   },
   plugins: [],
