48e7219d61df0ed33caf19dd3305a7d76429f85f
talented-star <zhiqiang9981@gmail.com>
2024-08-09 19:51:20 +0900

    feat: Update the layout component
    Updated the layout component to display the sidebar and some elements.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,32 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 import { Footer } from "@/components/Footer";
-import './globals.css';
-import Header from '@/components/Header';
-
+import "./globals.css";
+import Header from "@/components/Header";
+import SideBar from "@/components/SideBar";
 
 export const metadata: Metadata = {
-  title: 'Consolidation SAAS',
-  description: 'This website is dashboard management system for the shop manager',
-}
+  title: "Consolidation SAAS",
+  description:
+    "This website is dashboard management system for the shop manager",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <Header title={"DashBoard"}/>
-          {children}
-        <Footer/>
+        <div className="flex">
+          <SideBar />
+          <div>
+            <Header title={"DashBoard"} />
+            {children}
+          </div>
+        </div>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
