4b769f013937489ac309d179276b65414132b30b
talented-star <zhiqiang9981@gmail.com>
2024-08-09 19:50:14 +0900

    feat: Update the tailwind.config
    Updated the tailwind.config to add the some font style.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
 
+const plugin = require('tailwindcss/plugin');
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -19,8 +20,16 @@ const config: Config = {
         mono: ['Courier New', 'monospace'],
         robot: ["Roboto Condensed", 'sans-serif']
       },
+      boxShadow: {
+        'custom-top': '0 -5px 10px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)',
+        'custom-right': '4px 0 6px -1px rgba(0, 0, 0, 0.1), 2px 0 4px -1px rgba(0, 0, 0, 0.06)',
+        'custom-bottom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        'custom-left': '-4px 0 6px -1px rgba(0, 0, 0, 0.1), -2px 0 4px -1px rgba(0, 0, 0, 0.06)',
+      },      
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-textshadow'),
+  ],
 }
 export default config
