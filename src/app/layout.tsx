69b06288e17137e0cdf1a1dc18d0e5cd0331b092
talented-star <zhiqiang9981@gmail.com>
2024-08-09 10:13:16 +0900

    fix: Update the some code
    Update the layout and page .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Footer } from "@/components/Footer";
 import './globals.css';
+import Header from '@/components/Header';
 
 
 export const metadata: Metadata = {
@@ -16,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Header title={"DashBoard"}/>
+          {children}
         <Footer/>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 
 export default function Home() {
   return (
-    <>
-      <h1>Admin Panel</h1>
-    </>
+    <div className="p-10">
+      <Link href={"/subManage"} className="block hover:underline">Subscription Management</Link>
+      <Link href={"/integration"} className="block hover:underline">Integration</Link>
+      <Link href={"/analytics"} className="block hover:underline">Analytics</Link>
+      <Link href={"/userManage"} className="block hover:underline">User Management</Link>
+    </div>
   )
 };
