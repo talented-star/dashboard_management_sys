7e6de6e20c9698f24af222b90dee6f619e9b883a
talented-star <zhiqiang9981@gmail.com>
2024-08-10 20:30:18 +0900

    fix: Update the script
    Add the some logic to display some buttons.

--- src/components/SideBarButton.tsx
+++ src/components/SideBarButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { SideBarBtnProps } from "./Mockdata";
 import Link from "next/link";
+import { SideBarBtnProps } from "./Mockdata";
+import { PlusIcon, } from "lucide-react";
 
 const SideBarButton: React.FC<SideBarBtnProps> = ({
   label,
@@ -11,15 +12,25 @@ const SideBarButton: React.FC<SideBarBtnProps> = ({
   return (
     <>
       {setStyle === true ? (
-        <Link href={`/${link}`}>
-          <button className="p-4 m-4 shadow-md shadow-gray-400 rounded-[32px] flex justify-between items-center w-[184px] ">
-            {/* <div>{icon}</div> */}
-            {label}
-          </button>
-        </Link>
+        label === "Dashboard" ? (
+          <Link href={`/${link}`}>
+            <button className="p-4 m-4 shadow-md shadow-gray-400 rounded-[32px] flex justify-between items-center w-[184px] mt-[60px]">
+              {label}
+            </button>
+          </Link>
+        ) : (
+          <Link href={`/${link}`}>
+            <button className="p-1 m-4 shadow-md shadow-gray-400 rounded-[32px] flex justify-between items-center w-[184px]">
+              <div className="bg-[#7216F4] text-white rounded-full m-1">
+                <PlusIcon size={40}></PlusIcon>
+              </div>
+              <div className="text-left ml-2 text-black leading-normal">{label}</div>
+            </button>
+          </Link>
+        )
       ) : (
         <Link href={`/${link}`}>
-          <button className="p-4 m-4 text-[14px] w-[184px] border border-black rounded-[32px] flex justify-between items-center text-shadow-lg text-black shadow-md hover:shadow-xl">
+          <button className="p-4 m-4 text-[14px] w-[184px] hover:border hover:border-black rounded-[32px] flex justify-between items-center text-shadow-lg text-black shadow-sm hover:shadow-xl">
             {/* <div>{icon}</div> */}
             {label}
           </button>
