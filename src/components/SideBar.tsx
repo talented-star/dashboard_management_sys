da12a600318a1e3fd1c1934d14996d4c83bee0df
talented-star <zhiqiang9981@gmail.com>
2024-08-11 00:47:10 +0900

    fix: Update the sidebar
    Updated the sidebar component to display the sidebar.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { SideBarBtnProps } from './Mockdata';
 import SideBarButton from './SideBarButton';
-import { link } from 'fs';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import Dashboard from "../../public/dashboard.svg";
+import Tool from "../../public/tools.svg";
+import Subscription from "../../public/subscription.svg";
+import Integration from "../../public/integration.svg";
+import Reports from "../../public/reports.svg";
+import Security from "../../public/security.svg";
+import UserManagement from "../../public/userManagement.svg";
+
 
 
 const sideBarItemList: SideBarBtnProps[] = [
@@ -14,49 +22,52 @@ const sideBarItemList: SideBarBtnProps[] = [
     {
         label: "Dashboard",
         setStyle: true,
-        icon:"chart-line",
+        icon: Dashboard,
         link:"dashboard"
     },
     {
         label: "Tools",
         setStyle: false,
-        icon: "tooltip",
+        icon: Tool,
         link: "tool"
     },
     {
         label: "Subscriptions",
         setStyle: false,
-        icon: "info-circle",
+        icon: Subscription,
         link: "subscription"
     },
     {
         label: "Integrations",
         setStyle: false,
-        icon: "plug",
+        icon: Integration,
         link: "integration"
     },
     {
         label: "Reports",
         setStyle: false,
-        icon: "chart-bar",
+        icon: Reports,
         link: "reports"
     },
     {
         label: "User Management",
         setStyle: false,
-        icon: "user-cog",
+        icon: UserManagement,
         link: "userManage"
     },
     {
         label: "Security",
         setStyle: false,
-        icon: "security",
+        icon: Security,
         link: "security"
     }
 ]
 const SideBar: React.FC = () => {
     return (
-        <div style={{width : "260px !important"}} className='border-r'>
+        <div style={{width : "260px !important"}} className='border-r relative pt-[140px] px-7'>
+            <button className="top-8 -right-5 absolute border rounded-full p-2 bg-white text-black shadow-md hover:bg-gray-200 hover:text-white hover:shadow-lg">
+                <ChevronLeft />
+            </button>
             {
                 sideBarItemList.map((item, index) => (
                     <SideBarButton label={item.label} setStyle={item.setStyle} icon={item.icon} link={item.link} key={index}></SideBarButton>
